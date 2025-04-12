5b2e833609e2ee5fb6c6c69935ee647586f8a426
Peter <peaker122@gmail.com>
2025-04-12 19:28:21 +0200

    components are added

--- MovieMuse/src/main.tsx
+++ MovieMuse/src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
-import '@popperjs/core/dist/umd/popper';
-import 'bootstrap/dist/js/bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
 import { BrowserRouter } from 'react-router-dom';
+import '../src/components/Utilities.css'
 
 
 import './index.css'
