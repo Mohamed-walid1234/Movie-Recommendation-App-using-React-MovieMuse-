1a643151f140fc58fe598e92f5bf94bac6598056
waled <waledmohamed2004@gmail.com>
2025-04-12 14:54:50 +0200

    add files movies and css

---
+++ MovieMuse/src/pages/movies/Movies.tsx
@@ -0,0 +1,10 @@
+import style from "./Movies.module.css"
+function Movies() {
+    return (
+    <>
+        <h1 className={style.color}>test style</h1>
+    </>  
+    );
+}
+
+export default Movies;
