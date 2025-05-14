b55011a80cb440e0757e537759bda45671604136
Peter <peaker122@gmail.com>
2025-05-14 23:16:13 +0300

    Added 2 Pages

--- frontend/MovieMuse/src/main.tsx
+++ frontend/MovieMuse/src/main.tsx
@@ -11,8 +11,12 @@ import './index.css'
 
 // the app
 import App from './App.tsx'
+import Movie_Page from './pages/movie_page/Movie_Page.tsx';
+import Profile from './pages/profile/Profile.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <App />
+    <Profile />
+    <Movie_Page />
+    <App />
   </StrictMode>
 )
