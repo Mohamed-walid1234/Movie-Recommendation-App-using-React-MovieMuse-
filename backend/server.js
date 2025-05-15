53b743b1caf3d439e914b7e9a6c3c7050bd9f15d
Waled <waledmohamed2004@gmail.com>
2025-05-15 19:09:57 +0300

    t

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 import { connectDB } from "./config/db.js";
 import userRoute from "./routes/users.route.js";
 import moviesRoute from "./routes/movies.route.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded());
-
-
+app.use(cors());
 // user route
 app.use("/api/profile", userRoute);
 app.use("/api/movies", moviesRoute);    
